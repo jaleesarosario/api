3dcdcde5490d0e4e76eb12ccef1f68a522ec1ec1
JRosario <34451372+jaleesarosario@users.noreply.github.com>
2019-04-14 19:27:31 -0400

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+var config = {
+  NYT_KEY : "LfA9nbXITwPzCOiAGvtSauu9fvNlFN1v",
+  GOOGLE_BOOKS_KEY : "AIzaSyDgP2Kvj-UW7V9x6spnlzS0rbdxQyiayqc"
+}
+
+
+
+// Download a new NYT API key for the books - then add to this code
